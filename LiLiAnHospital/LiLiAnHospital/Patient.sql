fe987db4f0c6e769a7729e8e2bb5403d02aa68d9
Andreas Brandt <andreasbrandt@gmail.com>
2016-03-24 14:38:59 +0100

    Rearange fields in Patient

--- LiLiAnHospital/LiLiAnHospital/Patient.sql
+++ LiLiAnHospital/LiLiAnHospital/Patient.sql
@@ -2,10 +2,10 @@
 (
 	[patientID] INT NOT NULL PRIMARY KEY, 
     [firstName] NVARCHAR(50) NOT NULL, 
-    [lastName] NVARCHAR(50) NOT NULL, 
+    [lastName] NVARCHAR(50) NOT NULL,
+	[birthDate] DATE NOT NULL, 
+    [controlDigits] NCHAR(4) NOT NULL,
     [medicalSymptom] NVARCHAR(100) NULL, 
     [roomID] INT NULL, 
-    [diagnoseID] INT NULL, 
-    [birthDate] DATE NOT NULL, 
-    [controlDigits] NCHAR(4) NOT NULL
+    [diagnoseID] INT NULL
 )
