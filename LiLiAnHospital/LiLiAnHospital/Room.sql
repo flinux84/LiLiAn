cc4af905cf8b38f85bb1d18fcde7c6941d12bd5b
flinux84 <linus.soderholm@gmail.com>
2016-03-24 14:14:02 +0100

    added room-table

---
+++ LiLiAnHospital/LiLiAnHospital/Room.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Room]
+(
+	[roomID] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [departmentID] INT NOT NULL, 
+    [numOfBeds] INT NOT NULL DEFAULT 1, 
+    CONSTRAINT [FK_Room_Department] FOREIGN KEY ([departmentID]) REFERENCES [Department]([departmentID])
+)
