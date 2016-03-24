ef14e88300dcc049f254fbae13ea3fbe04caf980
Andreas Brandt <andreasbrandt@gmail.com>
2016-03-24 15:49:08 +0100

    Added diagnoseID as a foreign key in Farmaceutical

--- LiLiAnHospital/LiLiAnHospital/Diagnose.sql
+++ LiLiAnHospital/LiLiAnHospital/Diagnose.sql
@@ -2,8 +2,6 @@
 (
 	[diagnoseID] INT NOT NULL PRIMARY KEY IDENTITY, 
     [diagnoseName] NVARCHAR(50) NOT NULL, 
-    [farmaceuticalID] INT NOT NULL, 
     [departmentID] INT NOT NULL, 
-    CONSTRAINT [FK_Diagnose_Farmaceutical] FOREIGN KEY ([farmaceuticalID]) REFERENCES [Farmaceutical]([farmaceuticalID]), 
     CONSTRAINT [FK_Diagnose_Department] FOREIGN KEY ([departmentID]) REFERENCES [Department]([departmentID])
 )
--- LiLiAnHospital/LiLiAnHospital/Farmaceutical.sql
+++ LiLiAnHospital/LiLiAnHospital/Farmaceutical.sql
@@ -5,5 +5,7 @@
     [brand] NVARCHAR(100) NOT NULL, 
     [manufacturer] NVARCHAR(50) NOT NULL, 
     [medicalUse] NVARCHAR(150) NOT NULL, 
-    [patent] DATE NOT NULL
+    [patent] DATE NOT NULL, 
+    [diagnoseID] INT NOT NULL, 
+    CONSTRAINT [FK_Farmaceutical_Diagnose] FOREIGN KEY ([diagnoseID]) REFERENCES [Diagnose]([diagnoseID])
 )
