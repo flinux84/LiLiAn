af0ef91f2b771a06415bfdf9b5905186c0227664
flinux84 <linus.soderholm@gmail.com>
2016-03-24 14:50:36 +0100

    changed order on columns in doctor/nurse

--- LiLiAnHospital/LiLiAnHospital/Nurse.sql
+++ LiLiAnHospital/LiLiAnHospital/Nurse.sql
@@ -1,10 +1,10 @@
 ﻿CREATE TABLE [dbo].[Nurse]
 (
 	[nurseID] INT NOT NULL PRIMARY KEY, 
-    [birthDate] DATE NOT NULL, 
-	[controlDigits] NCHAR(4) NOT NULL,
     [firstName] NVARCHAR(50) NOT NULL, 
     [lastName] NVARCHAR(50) NOT NULL, 
+    [birthDate] DATE NOT NULL, 
+	[controlDigits] NCHAR(4) NOT NULL,
     [departmentID] INT NOT NULL, 
     CONSTRAINT [FK_Nurse_Department] FOREIGN KEY ([departmentID]) REFERENCES [Department]([departmentID]) 
     
