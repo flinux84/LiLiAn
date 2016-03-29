5869a572b0a617c150d10b63d7e3a5a2781af973
Andreas Brandt <andreasbrandt@gmail.com>
2016-03-29 09:54:38 +0200

    Stored procedure for available rooms in department

---
+++ LiLiAnHospital/LiLiAnHospital/CLRstoredProcedures/spAvailableRoomsInDepartment.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using Microsoft.SqlServer.Server;
+
+public partial class StoredProcedures {
+    /* List free rooms in a department */
+
+    [Microsoft.SqlServer.Server.SqlProcedure]
+    public static void spAvailableRoomsInDepartment(string departmentName) {
+
+        SqlParameter departmentParameter = new SqlParameter("@departmentName", SqlString.Null);
+        departmentParameter.Value = departmentName;
+
+        string procedureText =
+            @"
+            SELECT *
+            FROM
+                (SELECT Room.roomID, Room.numOfBeds, (Room.numOfBeds - COUNT(*)) AS numFreeBeds, Department.departmentName
+                FROM Room
+                JOIN Patient ON Patient.roomID = Room.roomID
+                JOIN Department On Department.departmentID = Room.departmentID
+                GROUP BY Room.roomID, Department.departmentName, Room.numOfBeds) AS NumFreeTable
+            WHERE NumFreeTable.numFreeBeds > 0 AND (departmentName = @departmentName);
+            ";
+
+        using (SqlConnection connection = new SqlConnection("context connection=true")) {
+            connection.Open();
+            SqlCommand command = new SqlCommand(procedureText, connection);
+            command.Parameters.Add(departmentParameter);
+            SqlDataReader reader = command.ExecuteReader();
+            SqlContext.Pipe.Send(reader);
+        }
+    }
+}
+
